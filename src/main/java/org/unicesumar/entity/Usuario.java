efd403bf9846af049c2a16608add78d3d3bbdf47
FelipeBarreirosMga <felipebarreirosmaringa@gmail.com>
2025-04-12 17:10:51 -0300

    add usuario

--- src/main/java/org/unicesumar/entity/Usuario.java
+++ src/main/java/org/unicesumar/entity/Usuario.java
@@ -1,9 +1,49 @@
 package org.unicesumar.entity;
 
+import jakarta.persistence.*;
+
+@Table(name = "usuarios")
 public class Usuario {
-    long id;
-    String nome;
-    String email;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(name = "nome")
+    private String nome;
+
+    @Column(name = "email")
+    private String email;
+
+    public Usuario() {}
+
+    public Usuario(long id, String nome, String email) {
+        this.id = id;
+        this.nome = nome;
+        this.email = email;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
-
-
