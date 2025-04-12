3b9db2d98722ac196ccd2396806416e057ef3b7c
FelipeBarreirosMga <felipebarreirosmaringa@gmail.com>
2025-04-12 18:12:06 -0300

    addvenda

--- src/main/java/org/unicesumar/entity/Venda.java
+++ src/main/java/org/unicesumar/entity/Venda.java
@@ -1,8 +1,61 @@
 package org.unicesumar.entity;
 
+import jakarta.persistence.*;
+
+@Table (name= "vendas")
 public class Venda {
-    long id;
-    Produto id_produto;
-    int quantidade;
-    double preco;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @JoinColumn(name = "id_produto", referencedColumnName = "id")
+    private Produto id_produto;
+
+    @Column(name = "quantidade")
+    private int quantidade;
+
+    @Column(name = "preco)")
+    private double preco;
+
+    public Venda() {}
+
+    public Venda(Produto id_produto, long id, int quantidade, double preco) {
+        this.id_produto = id_produto;
+        this.id = id;
+        this.quantidade = quantidade;
+        this.preco = preco;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public Produto getId_produto() {
+        return id_produto;
+    }
+
+    public void setId_produto(Produto id_produto) {
+        this.id_produto = id_produto;
+    }
+
+    public int getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(int quantidade) {
+        this.quantidade = quantidade;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(double preco) {
+        this.preco = preco;
+    }
 }
