f31c6d66feeb987df62f60045fc78d96c31b3914
FelipeBarreirosMga <felipebarreirosmaringa@gmail.com>
2025-04-12 17:31:29 -0300

    add produto

--- src/main/java/org/unicesumar/entity/Produto.java
+++ src/main/java/org/unicesumar/entity/Produto.java
@@ -1,7 +1,49 @@
 package org.unicesumar.entity;
 
+import jakarta.persistence.*;
+
+@Table(name = "produtos")
 public class Produto {
-    long id;
-    String nome;
-    Double preco;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(name = "nome")
+    private String nome;
+
+    @Column(name= "preco")
+    private Double preco;
+
+    public Produto() {}
+
+    public Produto(long id, String nome, Double preco) {
+        this.id = id;
+        this.nome = nome;
+        this.preco = preco;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public Double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(Double preco) {
+        this.preco = preco;
+    }
 }
